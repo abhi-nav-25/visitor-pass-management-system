60d505f6d8c699f63518f2b55f65cf0b1bd6240b
abhi-nav-25 <abhinav252006@gmail.com>
2026-06-16 00:59:10 +0530

    feat: integrate dashboard with backend APIs and authentication

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,44 @@
 import DashboardLayout from "../layouts/DashboardLayout";
 import StatCard from "../components/StatCard";
-import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import { useContext,useState, useEffect } from "react";
+import API from "../services/api";
 
 function Dashboard() {
   const { logout } = useContext(AuthContext);
+  const [visitorCount, setVisitorCount] = useState(0);
+  const [workerCount, setWorkerCount] = useState(0);
+  const [passCount, setPassCount] = useState(0);
+  const [logCount, setLogCount] = useState(0);
   const navigate = useNavigate();
   const handleLogout = () => {
     logout();
     navigate("/");
   };
+  const [recentVisitors, setRecentVisitors] = useState([]);
+  const [recentLogs, setRecentLogs] = useState([]);
+
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      try {
+        const visitors = await API.get("/visitors");
+        const workers = await API.get("/workers");
+        const passes = await API.get("/passes");
+        const logs = await API.get("/logs");
+        setVisitorCount(visitors.data.length);
+        setWorkerCount(workers.data.length);
+        setPassCount(passes.data.length);
+        setLogCount(logs.data.logs.length);
+        setRecentVisitors(visitors.data);
+        setRecentLogs(logs.data.logs);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchDashboardData();
+  }, []);
+
   return (
     <DashboardLayout>
       <h1>Dashboard</h1>
@@ -18,19 +46,70 @@ function Dashboard() {
           Logout
         </button>
         <div style={{display: "grid",gridTemplateColumns: "repeat(4, 1fr)",gap: "20px",marginTop: "20px"}}>
-            <StatCard title="Total Visitors" value="120" />
-            <StatCard title="Total Workers" value="45" />
-            <StatCard title="Active Passes" value="87" />
-            <StatCard title="People Inside" value="32" />    
+            <StatCard title="Total Visitors" value={visitorCount} />
+            <StatCard title="Total Workers" value={workerCount} />
+            <StatCard title="Active Passes" value={passCount} />
+            <StatCard title="People Inside" value={logCount} />    
         </div>
 
-        <div style={{marginTop: "30px",background: "white",padding: "20px",borderRadius: "10px"}}>
-            <h3>Recent Visitors</h3>
-        </div>
+        <div
+          style={{
+            marginTop: "30px",
+            background: "white",
+            padding: "20px",
+            borderRadius: "10px",
+          }}
+        >
+        <h3>Recent Visitors</h3>
+        <table style={{ width: "100%", marginTop: "15px" }}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Mobile</th>
+              <th>Purpose</th>
+            </tr>
+          </thead>
+          <tbody>
+            {recentVisitors.map((visitor) => (
+              <tr key={visitor._id}>
+                <td>{visitor.name}</td>
+                <td>{visitor.mobile}</td>
+                <td>{visitor.purpose}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table> 
+      </div>
 
-        <div style={{marginTop: "20px",background: "white",padding: "20px",borderRadius: "10px"}}>
-            <h3>Recent Entry Logs</h3>
-        </div>
+      <div
+        style={{
+          marginTop: "20px",
+          background: "white",
+          padding: "20px",
+          borderRadius: "10px",
+        }}
+      >
+        <h3>Recent Entry Logs</h3>
+        <table style={{ width: "100%", marginTop: "15px" }}>
+          <thead>
+            <tr>
+              <th>Entry Time</th>
+              <th>Exit Time</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {recentLogs.map((log) => (
+              <tr key={log._id}>
+                <td>{new Date(log.entryTime).toLocaleString()}</td>
+                <td>{log.exitTime ? new Date(log.exitTime).toLocaleString() : "Still Inside"}</td>
+                <td>{log.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </DashboardLayout>
   );
 }
