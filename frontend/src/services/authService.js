9e45f30ae2aee65d832ade6c37b72d9f937f7522
abhi-nav-25 <abhinav252006@gmail.com>
2026-06-25 11:43:33 +0530

    Fix production auth URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.VITE_API_URL + "/auth";
 
 export const loginUser = async (userData) => {
   const response = await axios.post(
