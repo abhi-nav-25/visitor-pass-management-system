0ebf6d2878813ecddc285bb582379d4b42ac9049
abhi-nav-25 <abhinav252006@gmail.com>
2026-06-25 13:58:41 +0530

    Configure production CORS

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,15 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://visitor-pass-management-system-pi.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(helmet());
 app.use(express.json());
